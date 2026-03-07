e4f60be6514f85a1089517654586b7133cc31680
CrisDFL <cristhiandavidfl@gmail.com>
2026-03-07 12:26:41 -0500

    uso de funciones

---
+++ Modulo-1/funciones-calcularArea/tarea/grocery.js
@@ -0,0 +1,9 @@
+
+
+calcular = (a, b, c) =>{
+    num1 = parseFloat(a.value)
+    num2 = parseFloat(b.value)
+    num3 = parseFloat(c.value)
+
+    document.getElementById("res").innerHTML = num1 + num2 + num3;
+}
